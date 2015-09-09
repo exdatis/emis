8bfa9a7972765d660d437dfdec24ea32afc0eb9a
exdatis <exdatis@gmail.com>
2015-09-09 21:25:47 +0200

    Prepare for ubuntu.

--- fdbBcp/emisRestore.sh
+++ fdbBcp/emisRestore.sh
@@ -6,6 +6,6 @@ echo "=============================================================="
 
 
 
-gbak -replace -v /home/exdatis/emis/fdbBcp/emis25.fbk localhost:/home/exdatis/exdatis/emis25.fdb -user sysdba -password masterkey
+gbak -replace -v /home/morar/emis/fdbBcp/emis25.fbk localhost:/home/morar/exdatis/emis25.fdb -user sysdba -password masterkey
 
 # read -p "Press [Enter] key to exit..."
